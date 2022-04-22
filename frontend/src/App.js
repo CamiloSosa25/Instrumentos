e4911f15e1e0acc068a0d3dab9c41bf8a71ecdbf
Camilo Andres <andresrnx2@gmail.com>
2022-04-21 19:10:56 -0500

    primer commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Instrumentos</a>
+      </header>
+      Lista de Items
+    </div>
+  );
+}
+
+export default App;
